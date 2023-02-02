edab8df6f67d7fc91112ff517ad70750fd188db0
lmhazel <leahazel2000@gmail.com>
2023-02-02 11:46:05 -0800

    completed read functionality

--- src/pages/CatShow.js
+++ src/pages/CatShow.js
@@ -1,11 +1,27 @@
-import React from "react"
+import React from 'react'
+import { useParams } from 'react-router-dom'
 
-const CatShow = () => {
+const CatShow = ({ cats }) => {
+
+  const { id } = useParams()
+  let selectedCat = cats.find(cat => cat.id === +id)
+
   return(
-    <>
-     <h1> CatShow</h1>
-    </>
-  )
+      <main className="cat-show-cards">
+        {selectedCat && (
+          <>
+            <h2>{selectedCat.name}</h2>
+            <img
+              alt={` ${selectedCat.name}'s profile`}
+              src={selectedCat.image}
+              className="cat-show-img"
+            />
+            <h3>{selectedCat.age} years old</h3>
+            <h3>{selectedCat.enjoys}</h3>
+          </>
+        )}
+      </main>
+    )
 }
 
-export default CatShow
+export default CatShow;
