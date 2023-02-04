e08e1b95cebffa81525f18c427681c3793ac3324
mollykwolf <mollykwolf@gmail.com>
2023-02-03 16:44:09 -0800

    completed fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import CatEdit from "./pages/CatEdit"
@@ -8,20 +8,42 @@ import CatShow from "./pages/CatShow"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import "./App.css"
-import mockCats from "./mockCats"
+// import mockCats from "./mockCats"
 import { Routes, Route } from "react-router-dom"
 
 
 const App = () => {
-  const [cats, setCats] = useState(mockCats)
+  const [cats, setCats] = useState([])
 
-  const createCat = (cat) => {
-    console.log(cat)
+  useEffect(() => {
+    readCat()
+  },[])
+  const readCat = () => {
+    fetch("http://localhost:3000/cats")
+      .then((response) => response.json())
+      .then((payload) => {
+        setCats(payload)
+      })
+      .catch((error) => console.log(error))
   }
+  const createCat = (cat) => {
+    fetch("http://localhost:3000/cats" , {
+      body: JSON.stringify(cat), 
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method:"POST"
+    })
+    .then((response) => response.json())
+    .then((payload) => readCat())
+    .catch((errors) => console.log("Cat create errors:", errors))
+  }
+  
   const updateCat = (cat, id) => {
     console.log("cat:", cat)
     console.log("id:", id)
   }
+
 
   return (
     <>
